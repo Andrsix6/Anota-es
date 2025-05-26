0c51f1465007221314e53a0ef86990ea197cb205
Andrsix6 <andrevfs10@gmail.com>
2025-05-25 22:51:41 -0300

    Create F-string.py

---
+++ Python/Hashtag/F-string.py
@@ -0,0 +1,10 @@
+faturamento = 2000
+custo = 500
+
+lucro = faturamento - custo
+
+# com format
+print ('O faturamento foi de {} e o lucro de {}'.format(faturamento, lucro))
+
+# com f-string
+print (f"O faturamento foi de {faturamento} e o lucro de {lucro}")
